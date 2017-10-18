ca2970e392762fa57827e86c177e26d58c1fdb4c
Pavao Vlahovic <pvlahovic@foi.hr>
2017-10-19 00:34:10 +0200

    API initial commit

---
+++ api/src/test/java/hr/foi/airprojekt/AirprojektApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.foi.airprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirprojektApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
