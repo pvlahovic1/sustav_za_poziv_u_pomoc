45dfe41e7ce82c0d8c8ad743356fd36d1503796e
Pavao Vlahovic <pvlahovic@foi.hr>
2017-10-21 20:41:27 +0200

    TK#47 Konfiguracija logova

--- api/src/main/java/hr/foi/airprojekt/controller/rest/KorisnikRestController.java
+++ api/src/main/java/hr/foi/airprojekt/controller/rest/KorisnikRestController.java
@@ -3,6 +3,7 @@ package hr.foi.airprojekt.controller.rest;
 import hr.foi.airprojekt.exception.KorisnikCredentialsException;
 import hr.foi.airprojekt.model.Korisnik;
 import hr.foi.airprojekt.service.KorisnikService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/rest-api")
 public class KorisnikRestController {
@@ -23,6 +25,7 @@ public class KorisnikRestController {
 
     @PostMapping("/korisnik/login")
     public Korisnik provideKorisnik(HttpServletResponse httpServletResponse, @RequestBody Map<String, String> map) throws IOException {
+        log.trace("POST /rest-api/korisnik/login");
         String mail = map.get("mail");
         String lozinka = map.get("lozinka");
         Korisnik korisnik = null;
@@ -31,9 +34,11 @@ public class KorisnikRestController {
             try {
                 korisnik = korisnikService.fetchKorisnikByMailAndLozinka(mail, lozinka);
             } catch (IllegalArgumentException e ) {
+                log.error("Invalid Username = {} or Password = {}", mail, lozinka);
                 httpServletResponse.sendError(400, e.getLocalizedMessage());
             }
         } else {
+            log.error("Bad request parameters mail={} lozinka= {}", mail, lozinka);
             httpServletResponse.sendError(400, "Bad request parameters [mail]=" + mail + " [lozinka]=" + lozinka);
         }
 
@@ -42,10 +47,13 @@ public class KorisnikRestController {
 
     @PostMapping("/korisnik")
     public void registerNewKorisnik(HttpServletResponse httpServletResponse, @RequestBody Korisnik korisnik) throws IOException {
+        log.trace("POST /rest-api/korisnik");
         try {
             korisnikService.registerNewKorisnik(korisnik);
         } catch (KorisnikCredentialsException e) {
+            log.error(e.getMessage(), e);
             httpServletResponse.sendError(400, e.getMessage());
         }
     }
 }
+
