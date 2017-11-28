160dbee119496f352085612624428d9be39e0b44
Pavao Vlahović <pvlahovic@foi.hr>
2017-11-29 00:52:53 +0100

    TK#43 Dodana google mapa na prikaz detalja o nesreći

--- web/src/main/resources/static/js/nesrece.js
+++ web/src/main/resources/static/js/nesrece.js
@@ -14,10 +14,37 @@ function showDetails(id) {
         type : 'GET',
         success : function(data) {
             $("#nesreca-modal-content").html(data);
+            myMap($("#xKoordinata").val(), $("#yKoordinata").val());
         },
         error : function(request,error)
         {
             console.log("Request: "+JSON.stringify(request));
         }
     });
+}
+
+function myMap(x, y) {
+    var myLatLng = {lat: parseFloat(x), lng: parseFloat(y)};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: myLatLng
+    });
+
+    var contentString = "<div>"+'Pozivni broj: '+ $("#brojMobitela").text() +"</div>";
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Poziv'
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
 }
