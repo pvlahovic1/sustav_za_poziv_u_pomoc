80d1aa983d946e65c810979193dd679d3ad0494b
Pavao Vlahović <pvlahovic@foi.hr>
2017-12-03 12:48:45 +0100

    Refactor - Unutar web i api aplikacija postavljeno lokalno vrijeme na Europe/Zagreb.

--- api/src/main/java/hr/foi/airprojekt/AirprojektApplication.java
+++ api/src/main/java/hr/foi/airprojekt/AirprojektApplication.java
@@ -3,10 +3,18 @@ package hr.foi.airprojekt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AirprojektApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirprojektApplication.class, args);
 	}
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Zagreb"));
+    }
 }
--- web/src/main/java/hr/foi/airprojekt/web/WebApplication.java
+++ web/src/main/java/hr/foi/airprojekt/web/WebApplication.java
@@ -6,12 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Zagreb"));
+    }
 
 	@Bean
     public PasswordEncoder providePasswordEncoder() {
