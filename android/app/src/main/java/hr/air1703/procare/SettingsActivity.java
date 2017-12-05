89ca66aa02a65db0ca3876bf5e09ed6824662563
cshad <georgemirat3@yahoo.com>
2017-12-06 00:35:33 +0100

    Refactor. Postavke UI je sada pravilno implementiran.

--- android/app/src/main/java/hr/air1703/procare/SettingsActivity.java
+++ android/app/src/main/java/hr/air1703/procare/SettingsActivity.java
@@ -1,13 +1,13 @@
 package hr.air1703.procare;
 
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.app_preferences);
     }
 }
