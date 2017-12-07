7908d96a719871457bc7edb10fbe6891c47fbec7
cshad <georgemirat3@yahoo.com>
2017-12-07 04:25:33 +0100

    TK#40 Implementirani zahtjevi za mape, dodan google map ključ, osnovni map fragment.

---
+++ android/map/src/main/java/hr/air1703/map/MapFragment.java
@@ -0,0 +1,43 @@
+package hr.air1703.map;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private int position;
+    private String name = "Map view";
+    private com.google.android.gms.maps.MapFragment mapFragment;
+    private GoogleMap map = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.map_fragment, container, false);
+        getFragmentManager().beginTransaction().add(R.id.frame, mapFragment).commit();
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapFragment.getMapAsync(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMyLocationEnabled(true);
+    }
+}
