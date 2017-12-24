b2037281b4e19e394bebd19ca95cf008da2ab3ee
Pavao Vlahovic <pvlahovic@foi.hr>
2017-12-24 13:27:14 +0100

    TK#91 Ispravljena greška prilikom slanja poziva u pomoć. Dodan uvijet u slučaju neuspjelog dohvaćanja razloga poziva u pomoć.

--- android/app/src/main/java/hr/air1703/procare/poziv/PozivService.java
+++ android/app/src/main/java/hr/air1703/procare/poziv/PozivService.java
@@ -10,6 +10,7 @@ import hr.air1703.database.model.Korisnik;
 import hr.air1703.database.model.Razlog;
 import hr.air1703.database.settings.LocalApplicationLog;
 import hr.air1703.procare.HelpCallActivity;
+import hr.air1703.procare.R;
 import hr.air1703.procare.utils.ApplicationUtils;
 import hr.air1703.webservice.remote.wrapper.PozivUPomocWrapper;
 
@@ -44,20 +45,24 @@ public abstract class PozivService {
             canSendRequest = true;
         }
 
-        if (canSendRequest) {
-            Location location = HelpCallActivity.location;
-            if (location != null) {
-                Korisnik korisnik = Korisnik.getAll().get(0);
+        if (razlog != null) {
+            if (canSendRequest) {
+                Location location = HelpCallActivity.location;
+                if (location != null) {
+                    Korisnik korisnik = Korisnik.getAll().get(0);
 
-                PozivUPomocWrapper poziv = new PozivUPomocWrapper(korisnik.getOib(),
-                        razlog.getNaziv(), location.getLatitude(), location.getLongitude());
+                    PozivUPomocWrapper poziv = new PozivUPomocWrapper(korisnik.getOib(),
+                            razlog.getNaziv(), location.getLatitude(), location.getLongitude());
 
-                PozivApi pozivApi = new PozivApi(pozivServiceHandler);
+                    PozivApi pozivApi = new PozivApi(pozivServiceHandler);
 
-                pozivApi.sendPozivUPomoc(poziv);
+                    pozivApi.sendPozivUPomoc(poziv);
+                }
+            } else {
+                pozivServiceHandler.onTimeDifferenceProblem(minTimeDiff - timeDiff);
             }
         } else {
-            pozivServiceHandler.onTimeDifferenceProblem(minTimeDiff - timeDiff);
+            pozivServiceHandler.onPozivFailure(R.string.error_razlog_nije_naveden);
         }
     }
 }
