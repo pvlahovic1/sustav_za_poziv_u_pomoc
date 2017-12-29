6f168d24638dccb55cae223e8b3b36e437924953
tadijam64 <tadijam64@gmail.com>
2017-12-29 01:12:44 +0100

    TK#88 Postavljanje notifikacije.

--- android/app/src/main/java/hr/air1703/procare/FirebaseMessagingService.java
+++ android/app/src/main/java/hr/air1703/procare/FirebaseMessagingService.java
@@ -1,5 +1,11 @@
 package hr.air1703.procare;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.RemoteMessage;
 
 /**
@@ -14,7 +20,26 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     }
 
     private void showNotification(String title, String message) {
-        //// TODO: 28.12.2017.
+        Context context = getBaseContext();
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        resultIntent.putExtra("title", title);
+        resultIntent.putExtra("body", message);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher_procare)
+                .setContentTitle(title)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+                .setContentText(message);
+
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, notificationBuilder.build());
     }
 
 }
