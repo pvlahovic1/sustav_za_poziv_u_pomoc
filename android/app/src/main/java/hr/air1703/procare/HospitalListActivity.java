91f3b73d05914aa7284e4192afc501dd09a9291a
cshad <georgemirat3@yahoo.com>
2018-01-05 00:32:00 +0100

    TK#40 Izmjene u postojećem kodu za omogućavanje prikaza karte

--- android/app/src/main/java/hr/air1703/procare/HospitalListActivity.java
+++ android/app/src/main/java/hr/air1703/procare/HospitalListActivity.java
@@ -2,8 +2,13 @@ package hr.air1703.procare;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hr.air1703.procare.fragments.HospitalListFragment;
 
 public class HospitalListActivity extends AppCompatActivity{
@@ -13,10 +18,18 @@ public class HospitalListActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_list);
 
+        ButterKnife.bind(this);
+
         HospitalListFragment mHospitalListFragment = new HospitalListFragment();
         FragmentManager mFragmentManager = getFragmentManager();
         FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.replace(R.id.fragment_container, mHospitalListFragment);
         mFragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.button_hospitalMap)
+    public void buttonHospitalMapClicked(View view){
+        Intent hospitalMapIntent = new Intent(HospitalListActivity.this, HospitalMapActivity.class);
+        HospitalListActivity.this.startActivity(hospitalMapIntent);
     }
 }
